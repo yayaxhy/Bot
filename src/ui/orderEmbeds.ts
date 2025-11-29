9a2620c34d668435526bfa56d22b3fdc59addb0e
YaYa <jamesfang1116@gmail.com>
2025-11-28 20:42:45 -0500

    Adjust thank-you embed footer spacing

--- src/ui/orderEmbeds.ts
+++ src/ui/orderEmbeds.ts
@@ -552,6 +552,7 @@ export function invite_successfully_inDiscord_embed(bossTag: string, workerTag: 
       `老板 ${bossTag} 已点单陪玩 ${workerTag}`,
       '',
       '💕祝老板玩的开心，期待下次相遇💕',
+      '',
       '<:_001_:1438676013860257943> https://jinleeclub.vip',
     ].join('\n'));
   const attachment = new AttachmentBuilder(THANK_BOSS_GIF_PATH).setName('thankBoss.gif');
@@ -568,6 +569,7 @@ export function anon_invite_successfully_inDiscord_embed(workerTag: string): Mes
       `匿名老板已点单陪玩 ${workerTag}`,
       '',
       '💕祝老板玩的开心，期待下次相遇💕',
+      '<:_001_:1438676013860257943> https://jinleeclub.vip',
     ].join('\n'));
   const attachment = new AttachmentBuilder(THANK_BOSS_GIF_PATH).setName('thankBoss.gif');
 
