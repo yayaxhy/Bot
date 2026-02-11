ed6f24df0bd85412c03cf0a9e3b54cd2d6f70d04
YaYa <jamesfang1116@gmail.com>
2026-02-10 22:24:55 -0500

    Update scratchEmbeds.ts

--- src/ui/scratchEmbeds.ts
+++ src/ui/scratchEmbeds.ts
@@ -11,22 +11,22 @@ const SCRATCH_EMBED_COLOR = 0xD4AF37;
 const SCRATCH_EMBED_THUMBNAIL_URL =
   'https://cdn.discordapp.com/attachments/1445864521343439019/1469920031272992768/2.gif?ex=69896935&is=698817b5&hm=165f9dc19c804078f011b9d78e725a6dccf267c3f4082b8b8a7d36a0f5d07d3d';
 const SCRATCH_PENDING_IMAGE_URL =
-  'https://media.discordapp.net/attachments/1445864521343439019/1470497390036386049/7d31db6718f4267f.png?ex=698b82ea&is=698a316a&hm=495484fb6b4a6eb370bf8bc92a6ef7e2b5f5e02a11b956972f4ae65b70bbdbe5&=&format=webp&quality=lossless&width=2249&height=1265';
+  'https://media.discordapp.net/attachments/1445864521343439019/1470678417971085412/7e0841ec8730be350fed995911ec0ee2.png?ex=698cd442&is=698b82c2&hm=67071052c42e58fd660b16afd5a8b5b322d0b93faedf524b7c76f76b87474df3&=&format=webp&quality=lossless&width=2249&height=1265';
 const SCRATCH_REVEAL_IMAGE_BY_TYPE: Partial<Record<ScratchPrizeType, string>> = {
   [ScratchPrizeType.THANKS]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470536534137504012/image.png?ex=698ba75e&is=698a55de&hm=8e166d51a49d9cb325858e4275c34313d5d4b82517f7256b893ad46e6c9d6126&=&format=webp&quality=lossless&width=2249&height=1265',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470668481241550889/014a4f08e851b85f.png?ex=698ccb01&is=698b7981&hm=eeb982c3d6377f61622a797fd60de7e5b747fb2e257e92ba4b25bb155a9736df&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P5]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470542058744709161/image.png?ex=698bac84&is=698a5b04&hm=25cf9f38b340e950c6bc75065724ed11c8f0e148ff516512889040df0121a788&=&format=webp&quality=lossless&width=2249&height=1265',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470657275730591825/5_1.png?ex=698cc092&is=698b6f12&hm=37ac7c82a13f6ca511f5cf787a7a7be5bcddb0133c88884952b0347dcee2ebcc&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P20]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470542099236655219/image.png?ex=698bac8d&is=698a5b0d&hm=5f1c9e1e951c8b21b44d0b91e6a757fed4ceaf7728855c4af2be59889ce93d55&=&format=webp&quality=lossless&width=2249&height=1265',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470658217695776798/20.png?ex=698cc172&is=698b6ff2&hm=7a2d75236af138781865e7cd63324af0ab8c37e3e01b29f33ae6020d57b80903&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P30]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470542159420850510/image.png?ex=698bac9c&is=698a5b1c&hm=7d579989c791ca72209fb89cc98357fccfb803cba0acbc5043df95679e725522&=&format=webp&quality=lossless&width=825&height=464',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470658556553334919/30.png?ex=698cc1c3&is=698b7043&hm=818194162808c08536383ebd4f6cffe9d9a0dc147aa79612e7cf741e7b7e93f6&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P50]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470542480922382468/image.png?ex=698bace8&is=698a5b68&hm=639b39d91c2a23b210ec378993322dc48bbe4dff2388bd9873d39d4f50e7f378&=&format=webp&quality=lossless&width=825&height=464',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470658936989417512/50.png?ex=698cc21e&is=698b709e&hm=a251553af2f49dadd9487d91b8b851389be1fdf9be8d0711f995411ff186c74e&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P99]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470542427235418182/image.png?ex=698bacdc&is=698a5b5c&hm=8774afda59ba1ffd2121728c9da6cdcf7253e1c86d8d52d4cfec2a8b762386dd&=&format=webp&quality=lossless&width=825&height=464',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470661628226830386/99.png?ex=698cc49f&is=698b731f&hm=bfeff945fc2cc92e85813f93beda0fed08132f87c7f4fc21ac139421eec9056b&=&format=webp&quality=lossless&width=2249&height=1265',
   [ScratchPrizeType.P200]:
-    'https://media.discordapp.net/attachments/1445864521343439019/1470536677251350720/image.png?ex=698ba781&is=698a5601&hm=200c37c959d755f9ad4e8a424135d41d53a03250c61f43379e2f6926e548792a&=&format=webp&quality=lossless&width=2249&height=1265',
+    'https://media.discordapp.net/attachments/1445864521343439019/1470666558782181480/150.png?ex=698cc937&is=698b77b7&hm=d7246c49794e8ed53f397bf948ae67a7afb9100512255dc52200fb96f54db6a4&=&format=webp&quality=lossless&width=2249&height=1265',
 };
 
 export function buildScratchRevealButton(ticketId: string, disabled = false) {
